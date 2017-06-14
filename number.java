e363b863625ff76a5011f2b1d4350ab93c5b8c8c
rohini1996 <rohini111222@gmail.com>
2017-06-14 12:16:51 +0530

    number.java

---
+++ number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+class number
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n>0)
+{
+System.out.println("number is positive ");
+}
+else if(n<0)
+{
+System.out.println("number is negative");
+}
+else
+{
+System.out.println("number is zero");
+}}}
